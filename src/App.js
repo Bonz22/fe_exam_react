cf67ad93c66b4b68ca3223fdc718b74b9411ae2a
Bonifacio L Del Rosario Jr <bldelrosario@sysnetph.com>
2021-12-16 11:26:20 +0800

    Initial commit for my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ExampleToast = ({ children }) => {
+    const [show, toggleShow] = useState(true);
+    const [storedEvents, setStoredEvents] = useState([]);
+
+    useEffect(()=>{
+        const url = 'http://localhost:3001/get';
+
+        const fetchData = async ()=>{
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                console.log(json);
+                setStoredEvents(json);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchData();
+    },[])
+
+    return (
+        storedEvents.map((data, index)=>(
+            <>
+                {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+                <Toast show={show} onClose={() => toggleShow(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">{data.event}</strong>
+                </Toast.Header>
+                <Toast.Body>{data.date_created}</Toast.Body>
+                </Toast>
+            </>
+        ))
+    );
+};
+
+const App = () => (
+    <Container className="p-3">
+        <Container className="p-5 mb-4 bg-light rounded-3">
+            <h1 className="header">Calendar</h1>
+            <ExampleToast>
+
+            </ExampleToast>
+        </Container>
+    </Container>
+);
 
 export default App;
